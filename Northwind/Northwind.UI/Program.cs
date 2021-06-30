27ba9f33e272c7e62815c2b38c03675945b7aac3
Jhobany Peña <jhobany.pena@hotmail.com>
2021-06-29 20:06:38 -0500

    Se configuro Sentry para el manejo de errores

--- Northwind/Northwind.UI/Program.cs
+++ Northwind/Northwind.UI/Program.cs
@@ -20,6 +20,17 @@ namespace Northwind.UI
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    // Add the following line:
+                    webBuilder.UseSentry(o =>
+                    {
+                        o.Dsn = "https://2884b1bcbaec4896a7d5e501bdae12fa@o892848.ingest.sentry.io/5840078";
+                        // When configuring for the first time, to see what the SDK is doing:
+                        o.Debug = true;
+                        // Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
+                        // We recommend adjusting this value in production.
+                        o.TracesSampleRate = 1.0;
+                    });
+
                     webBuilder.UseStartup<Startup>();
                 });
     }
